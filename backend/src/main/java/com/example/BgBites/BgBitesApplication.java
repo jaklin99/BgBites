30b025d7c2ae107cdbeec83419ff0694930b0901
jaklin99 <jaklin.yanakieva@gmail.com>
2025-05-09 02:13:57 +0200

    backend initialisation

---
+++ backend/src/main/java/com/example/BgBites/BgBitesApplication.java
@@ -0,0 +1,13 @@
+package com.example.BgBites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgBitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgBitesApplication.class, args);
+	}
+
+}
