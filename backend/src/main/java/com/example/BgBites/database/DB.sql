d9779f4f6cb30b87e8144fe95645617cbe0b8c96
jaklin99 <jaklin.yanakieva@gmail.com>
2025-05-11 00:35:50 +0200

    deleting backend folder

--- backend/src/main/java/com/example/BgBites/database/DB.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE recipes_db;
--- backend/src/main/java/com/example/BgBites/repositories/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BgBites.repositories;
-
-import com.example.BgBites.models.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
